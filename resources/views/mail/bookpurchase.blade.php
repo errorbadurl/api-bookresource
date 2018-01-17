d6d3507ddd70024e8050668650f1b5d923be6473
Earl G R <egrubellano@gmail.com>
2018-01-17 17:25:38 +0800

    updates:
    - updated email view for success purchase

--- resources/views/mail/bookpurchase.blade.php
+++ resources/views/mail/bookpurchase.blade.php
@@ -3,28 +3,13 @@
     <head>
         <meta charset="utf-8">
         <title>API Book Resource - Purchase</title>
-        <!-- Bootstrap -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-        </style>
     </head>
-    <body style="background-color: #fff">
-        <div class="fluid-container">
-            <h1 class="text-success">Purchase Successful</h1>
-            <div class="col-lg-12">
-                <h4>Puchase Summary :</h4>
-                <ul>
+    <body style="background-color:#fff;font-family:'Raleway',sans-serif;width:100%">
+        <div style="padding:10px;">
+            <h1 style="font-family:'Raleway',sans-serif;font-size:32px;">Purchase Successful</h1>
+            <div>
+                <h2 style="color:#4caf50;font-weight:100;font-family:'Raleway',sans-serif;font-size:25px;margin-bottom:0px;margin-top:0px;padding:0 20px;">Puchase Summary :</h2>
+                <ul style="line-height:3;">
                     <li>Customer Name : {{$user->first_name}} {{$user->last_name}}</li>
                     <li>Customer Email : {{$user->email}}</li>
                     <li>Book Title : {{$book->title}}</li>
@@ -33,7 +18,7 @@
                     <li>Quantity : {{$quantity}}</li>
                 </ul>
             </div>
-            <h3 class="text-danger"><i>Thank you for your purchase!</i></h3>
+            <h2 style="color:#f44336;font-family:'Raleway',sans-serif;font-size:25px;font-style:italic;font-weight:700;">Thank you for your purchase!</h2>
         </div>
     </body>
 </html>
